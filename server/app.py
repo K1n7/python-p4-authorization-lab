8a8a3ad162a570ce4b9aa33201d2826cbbadfacc
Tom Amollo <iantom96@gmail.com>
2025-06-06 23:41:57 +0300

    Update app.py

--- server/app.py
+++ server/app.py
@@ -87,12 +87,24 @@ class CheckSession(Resource):
 class MemberOnlyIndex(Resource):
     
     def get(self):
-        pass
+        if not session.get('user_id'):
+            return {'error': 'Unauthorized access. Please log in.'}, 401
+        
+        articles = Article.query.filter_by(is_member_only=True).all()
+        articles_json = [article.to_dict() for article in articles]
+        return make_response(jsonify(articles_json), 200)
 
 class MemberOnlyArticle(Resource):
     
     def get(self, id):
-        pass
+        if not session.get('user_id'):
+            return {'error': 'Unauthorized access. Please log in.'}, 401
+        
+        article = Article.query.filter_by(id=id, is_member_only=True).first()
+        if not article:
+            return {'error': 'Article not found or unauthorized.'}, 404
+        
+        return make_response(jsonify(article.to_dict()), 200)
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(IndexArticle, '/articles', endpoint='article_list')
